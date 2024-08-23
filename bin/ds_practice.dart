d6904abf1bf35785495686b41746c1620ad7107c
Dennizx <djconnect189@gmail.com>
2024-08-23 18:35:14 +0530

    DF

--- bin/ds_practice.dart
+++ bin/ds_practice.dart
@@ -16,6 +16,8 @@ class Heap {
   }
 }
 
+//! MIN
+
 class MinHeap extends Heap {
   void build(List<int> arr) {
     heap = arr;
@@ -64,4 +66,80 @@ class MinHeap extends Heap {
       heapifyDown(smallest);
     }
   }
+}
+
+//! MAX
+
+class MaxHeap extends Heap {
+  void build(List<int> arr) {
+    heap = arr;
+    for (int i = parent(heap.length - 1); i >= 0; i--) {
+      heapifyDown(i);
+    }
+  }
+
+  void insert(int value) {
+    heap.add(value);
+    heapifyUp(heap.length - 1);
+  }
+
+  int remove() {
+    if (heap.isEmpty) {
+      throw Exception('Heap is empty');
+    }
+    int root = heap[0];
+    heap[0] = heap.removeLast();
+    heapifyDown(0);
+    return root;
+  }
+
+  void heapifyUp(int index) {
+    while (index > 0 && heap[index] > heap[parent(index)]) {
+      swap(index, parent(index));
+      index = parent(index);
+    }
+  }
+
+  void heapifyDown(int index) {
+    int largest = index;
+    int left = leftChild(index);
+    int right = rightChild(index);
+
+    if (left < heap.length && heap[left] > heap[largest]) {
+      largest = left;
+    }
+
+    if (right < heap.length && heap[right] > heap[largest]) {
+      largest = right;
+    }
+
+    if (largest != index) {
+      swap(index, largest);
+      heapifyDown(largest);
+    }
+  }
+}
+
+
+
+void main() {
+  MinHeap minHeap = MinHeap();
+  minHeap.build([5, 3, 8, 4, 1, 2]);
+  minHeap.printHeap(); // Should print the heap in min-heap order
+
+  minHeap.insert(0);
+  minHeap.printHeap(); // After inserting 0
+
+  print(minHeap.remove()); // Removes and returns the smallest element (0)
+  minHeap.printHeap(); // Heap after removal
+
+  MaxHeap maxHeap = MaxHeap();
+  maxHeap.build([5, 3, 8, 4, 1, 2]);
+  maxHeap.printHeap(); // Should print the heap in max-heap order
+
+  maxHeap.insert(10);
+  maxHeap.printHeap(); // After inserting 10
+
+  print(maxHeap.remove()); // Removes and returns the largest element (10)
+  maxHeap.printHeap(); // Heap after removal
 }
