946d7ce5c9e6991637cd804cb73d7d035957a11f
Dennizx <djconnect189@gmail.com>
2024-08-26 13:19:31 +0530

    sd

--- bin/ds_practice.dart
+++ bin/ds_practice.dart
@@ -1,160 +1,79 @@
-class Node {
-  int data;
-  Node? left;
-  Node? right;
-  Node(this.data);
-}
-
-class BinarySearchTree {
-
-  Node? root;
-
-//! insert node
-  void insertNode(int data) {
-    Node? currentNode = root;
-    Node newNode = Node(data);
-
-    if(currentNode == null) {
-      root = newNode;
-      return;
-    }
-
-    while(true) {
-      if(newNode.data < currentNode!.data) {
-        if(currentNode.left == null) {
-          currentNode.left = newNode;
-          break;
-        } else {
-          currentNode = currentNode.left;
-        }
-      } else {
-        if(currentNode.right == null) {
-          currentNode.right = newNode;
-          break;
-        } else {
-          currentNode = currentNode.right;
-        }
-      }
-    }
-  }
-
-  
-//! check contains
-bool contains(int data) {
-  Node? currentNode = root;
-
-  while(currentNode != null) {
-    if(data < currentNode.data) {
-      currentNode = currentNode.left;
-    } else if(data > currentNode.data) {
-      currentNode = currentNode.right;
-    } else {
-      return true;
-    }
-  }
-  return false;
-}
-
-void deleteNode(int data) {
-  deleteHelper(data, root, null);
-}
-
-void deleteHelper(int data, Node? currentNode, Node? parentNode) {
-
-  while(currentNode != null) {
-    if(data < currentNode.data) {
-      parentNode = currentNode;
-      currentNode = currentNode.left;
-    } else if (data > currentNode.data) {
-      parentNode = currentNode;
-      currentNode = currentNode.right;
-    } else {
-      if(currentNode.left != null && currentNode.right != null) {
-        currentNode.data = getMinValueHelper(currentNode.right);
-        deleteHelper(currentNode.data, currentNode.right, currentNode);
-      } else {
-        if(parentNode == null) {
-          if(currentNode.right == null) {
-            root = currentNode.left;
-          } else {
-            root = currentNode.right;
-          }
-        } else {
-          if(parentNode.left == currentNode) {
-            if(currentNode.right == null) {
-              parentNode.left = currentNode.left;
-            } else {
-              parentNode.left = currentNode.right;
-            }
-          } else {
-            if(currentNode.right == null) {
-              parentNode.right = currentNode.left;
-            } else {
-              parentNode.right = currentNode.right;
-            }
-          }
-        }
-      }
-      break;
-    }
-  }
-}
-
-int getMinValueHelper(Node? currentNode) {
-  if(currentNode!.left == null) {
-    return currentNode.data;
-  } else {
-    return getMinValueHelper(currentNode.left);
-  }
-}
-
-//! inorder
-
-void inorder() {
-  inOrderHelper(root);
-}
-
-void inOrderHelper(Node? node) {
-  if(node != null) {
-  inOrderHelper(node.left);
-  print(node.data);
-  inOrderHelper(node.right);
-  }
-}
-
-void preOrder() {
-  preOrderHelper(root);
-}
-
-void preOrderHelper(Node? node) {
-  if(node != null) {
-    print(node.data);
-    preOrderHelper(node.left);
-    preOrderHelper(node.right);
-  }
-}
-
-  void postOrder() {
-    postOrderHelper(root);
-  }
-
-  void postOrderHelper(Node? node) {
-    if(node != null) {
-      postOrderHelper(node.left);
-      postOrderHelper(node.right);
-      print(node.data);
-    }
-  }
-}
-
-
 void main() {
-  BinarySearchTree tree = BinarySearchTree();
+  Graph grap = Graph();
+  grap.insert(10, 14);
+  grap.insert(10, 15);
+  grap.insert(19, 10);
+  grap.insert(10, 17);
+  grap.insert(10, 18);
+  grap.insert(17, 19);
+  grap.insert(17, 20);
+  grap.bfs(10);
+}
 
-  tree.insertNode(10);
-  tree.insertNode(30);
-  tree.insertNode(5);
+class Graph {
+  Map<int, List<int>> graph = {
+    1: [0, 3],
+    3: [1, 4],
+    0:[]
+  };
 
-  
-  
+  void insert(int vertix, int edge, [bool isbidirectional = false]) {
+    if (!graph.containsKey(vertix)) {
+      graph[vertix] = [];
+    }
+    if (!graph.containsKey(edge)) {
+      graph[edge] = [];
+    }
+    graph[vertix]!.add(edge);
+    if (isbidirectional) {
+      graph[edge]!.add(vertix);
+    }
+  }
+
+  void showedges(int vertix) {
+    if (graph.containsKey(vertix)) {
+      print(graph[vertix]);
+    }
+  }
+
+  bfs(int vertix) {
+    Set<int> visited = {};
+    List<int> que = [];
+
+    visited.add(vertix);
+    que.add(vertix);
+    while (que.isNotEmpty) {
+      int current = que.removeAt(0);
+      for (var i in graph[current]!) {
+        if (!visited.contains(i)) {
+          visited.add(i);
+          que.add(i);
+        }
+      }
+    }
+    print(visited);
+  }
+
+  dfs(int vertix) {
+    if (graph.containsKey(vertix)) {
+      Set<int> visited = {};
+      dfshelper(vertix, visited);
+      print(visited.toList());
+    }
+  }
+
+  dfshelper(int vertix, Set<int> visited) { 
+    visited.add(vertix);
+    for (var i in graph[vertix]!) {
+      if (!visited.contains(i)) {
+        dfshelper(i, visited);
+      }
+    }
+  }
+
+  void display() {
+    graph.forEach((key, vertix) {
+      print('$key:$vertix');
+    });
+  }
 }
